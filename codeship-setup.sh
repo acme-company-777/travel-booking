51771d27b9bc191df62fadbbff57d4298f6617b2
voltes-five <54273957+voltes-five@users.noreply.github.com>
2019-08-20 11:36:24 +0800

    .

---
+++ codeship-setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+# check if installed
+#function checker() { 
+#        which "$1" | grep -o "$1" > /dev/null &&  return 0 || return 1 
+#}
+
+function checker2() { 
+        dpkg -l "$1" | grep -o "$1" > /dev/null &&  return 0 || return 1 
+}
+
+if [ ! -f packages-microsoft-prod.deb ]; 
+  then wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb;
+    sudo dpkg -i packages-microsoft-prod.deb
+    sudo add-apt-repository universe
+fi 
+
+sudo apt-get update
+
+if checker2 "apt-transport-https" == 0 ; then echo "Installed"; else echo "Not Installed!"; fi
+if checker2 "dotnet-sdk-2.2" == 0 ; then echo "Installed"; else echo "Not Installed!"; fi
